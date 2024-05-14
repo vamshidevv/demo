65446f911121a0170a1c0bf911a593c40289f681
vamshidevv <vamshi.bhandari.5057@gmail.com>
2024-05-14 10:14:34 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Demo from "./Components/Demo";
 import ProfileInfo from "./Components/ProfileInfo";
+import Header from "./Components/Header";
+import Icons from "./Components/Icons";
 
 function App() {
   return (
     <>
+      <Header />
       <ProfileInfo />
       <Demo />
+      <Icons/>
     </>
   );
 }
--- src/Components/ProfileInfo.jsx
+++ src/Components/ProfileInfo.jsx
@@ -1,9 +1,27 @@
 import React, { useRef } from "react";
-import video from "../Images/ezgif-1-d13da8eb06.mp4"
+import video from "../Images/ezgif-1-d13da8eb06.mp4";
 import profile from "../Images/profile.jpg";
-import { IoCall } from "react-icons/io5";
-
+import CallIcon from '@mui/icons-material/Call';
 const ProfileInfo = () => {
+  const downloadContact = () => {
+    const vCardContent = `BEGIN:VCARD
+      VERSION:3.0
+      FN:SAGAR UMRETHIYA
+      TEL:+1234567890
+      END:VCARD`;
+
+    const blob = new Blob([vCardContent], { type: "text/vcard" });
+
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "contact.vcf");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  };
+
   const videoRef = useRef(null);
 
   const togglePlayPause = () => {
@@ -38,14 +56,13 @@ const ProfileInfo = () => {
               </div>
             </div>
             <div className="save-contact-container">
-              <a
-                href="https://www.whatsapp.com/contact/noclient?lang=en_US"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a target="_blank" rel="noopener noreferrer">
                 <div className="save-contact">
                   <div className="call-icon">
-                    <IoCall style={{ fontSize: "2rem" }} />
+                    <CallIcon
+                      style={{ fontSize: "2rem" }}
+                      onClick={downloadContact}
+                    />
                   </div>
                   <div className="contact-btn">
                     <button>Save Contact</button>
